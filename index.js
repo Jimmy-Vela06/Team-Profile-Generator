6238686b4504edfd95218986201682cfb0734ba4
Jimmy Vela <jimmyvela06@.com>
2022-10-26 02:16:51 -0500

    added inquierer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("hola world");
+
 const manager = require("./lib/manager.js");
 const engineer = require("./lib/engineer.js");
 const intern = require("./lib/intern.js");
@@ -5,3 +7,4 @@ const intern = require("./lib/intern.js");
 //node modules
 // const fs = require("fs");
 // const inquirer = require("inquirer");
+console.log("hola world");
