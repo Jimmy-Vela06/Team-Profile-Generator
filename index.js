df60799961d3536cc9807487c6bf4b8d13e93ed7
Jimmy Vela <jimmyvela06@.com>
2022-10-26 01:15:48 -0500

    starter lib file code

---
+++ index.js
@@ -0,0 +1,7 @@
+const manager = require("./lib/manager.js");
+const engineer = require("./lib/engineer.js");
+const intern = require("./lib/intern.js");
+
+//node modules
+// const fs = require("fs");
+// const inquirer = require("inquirer");
