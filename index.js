cf53713d3a1733fc0ebf05d34f8494f20fbd76e9
Jimmy Vela <jimmyvela06@.com>
2022-11-08 01:48:36 -0600

    index.js edits2

--- index.js
+++ index.js
@@ -11,65 +11,115 @@ const inquirer = require("inquirer");
 // generates HTML file
 const generateHtml = require("./utils/generateHTML.js");
 
+const team = [];
+
 const managerPrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter the team managers name.",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter mangers ID.",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter managers email",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Enter managers office number.",
-    },
-  ]);
-  //   .then()
+  console.log("\x1b[32m%s\x1b[0m", "BUILD YOUR TEAM");
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the team managers name.",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter mangers ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter managers email",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter managers office number.",
+      },
+    ])
+    .then((answers) => {
+      const { name, id, email, officeNumber } = answers;
+      const manager = new Manager(name, id, email, officeNumber);
+      team.push(manager);
+    });
 };
 
 const emoloyeePrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter the name of the employee.",
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "Choose the employee's role.",
-      choices: ["Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Enter employee's ID.",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter employee's email.",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter engineer's github username.",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "Enter intern's school.",
-    },
-  ]);
-  //.then
+  console.log("\x1b[32m%s\x1b[0m", `Adding employee to the team`);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the name of the employee.",
+      },
+      {
+        type: "list",
+        name: "role",
+        message: "Choose the employee's role.",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter employee's ID.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter employee's email.",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter engineer's github username.",
+        when: (input) => input.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter intern's school.",
+        when: (input) => input.role === "Intern",
+      },
+      {
+        type: "confirm",
+        name: "confirmAddEmployee",
+        message: "Would you like to add more team members?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      // data for employee types
+
+      let {
+        name,
+        id,
+        email,
+        role,
+        github,
+        school,
+        confirmAddEmployee,
+      } = employeeData;
+      let employee;
+
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+
+        console.log(employee);
+      }
+
+      team.push(employee);
+
+      if (confirmAddEmployee) {
+        return addEmployee(team);
+      } else {
+        return team;
+      }
+    });
 };
+emoloyeePrompt();
