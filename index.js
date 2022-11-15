d935ea670dd59f98d05ba02007c1943ff7b13f6c
Jimmy Vela <jimmyvela06@.com>
2022-11-15 02:26:40 -0600

    restructure code and fixes/ css and html customizing adds

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 // console.log("hola world");
 
-const manager = require("./lib/manager.js");
-const engineer = require("./lib/engineer.js");
-const intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
 
 // node modules
 const fs = require("fs");
+const path = require("path");
+
 const inquirer = require("inquirer");
 
-// generates HTML file
-// const generateHtml = require("./utils/generateHTML.js");
+const templates = require("./src/templates");
 
-const teamArray = [];
+const DIST = path.resolve(__dirname, "dist");
+const PATH = path.join(DIST, "team.html");
+
+const team = [];
+
+function main() {
+  managerPrompt().then((answers) => {
+    const { name, id, email, officeNumber } = answers;
+    const manager = new Manager(name, id, email, officeNumber);
+    team.push(manager);
+    teamPrompt();
+  });
+}
 
 const managerPrompt = () => {
   console.log("\x1b[32m%s\x1b[0m", "BUILD YOUR TEAM");
@@ -37,97 +50,150 @@ const managerPrompt = () => {
       message: "Enter managers office number.",
     },
   ]);
-  // .then((answers) => {
-  //   const { name, id, email, officeNumber } = answers;
-  //   const manager = new Manager(name, id, email, officeNumber);
-  //   teamArray.push(manager);
-  // });
 };
 
-const emoloyeePrompt = () => {
-  console.log("\x1b[32m%s\x1b[0m", `Adding employee to the team`);
+const engineerPrompt = () => {
+  console.log("\x1b[32m%s\x1b[0m", "Enter engineer information.");
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter the engineer's name.",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter engineer's ID.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter engineer's email",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter engineer's github user name.",
+    },
+  ]);
+};
+
+const internPrompt = () => {
+  console.log("\x1b[32m%s\x1b[0m", "Enter intern information.");
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter the inter's name.",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter intern's ID.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter intern's email",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter intern's school.",
+    },
+  ]);
+};
+
+const teamPrompt = () => {
+  console.log("\x1b[32m%s\x1b[0m", `Add team member to the team`);
   return inquirer
     .prompt([
       {
-        type: "input",
-        name: "name",
-        message: "Please enter the name of the employee.",
-      },
-      {
         type: "list",
-        name: "role",
-        message: "Choose the employee's role.",
-        choices: ["Engineer", "Intern"],
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "Enter employee's ID.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Enter employee's email.",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Enter engineer's github username.",
-        when: (input) => input.role === "Engineer",
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "Enter intern's school.",
-        when: (input) => input.role === "Intern",
-      },
-      {
-        type: "confirm",
-        name: "confirmAddEmployee",
-        message: "Would you like to add more team members?",
-        default: false,
+        name: "teamMemberChoice",
+        message: "Which team member would like to add?",
+        choices: ["Engineer", "Intern", "I've finished my team"],
       },
     ])
-    .then((employeeAnswers) => {
-      // data for employee types
+    .then((answer) => {
+      switch (answer.teamMemberChoice) {
+        case "Engineer":
+          engineerPrompt().then((answers) => {
+            const { name, id, email, github } = answers;
+            const engineer = new Engineer(name, id, email, github);
+            team.push(engineer);
+            teamPrompt();
+          });
+          break;
 
-      let {
-        name,
-        id,
-        email,
-        role,
-        github,
-        school,
-        confirmAddEmployee,
-      } = employeeAnswers;
-      let employee;
-
-      if (role === "Engineer") {
-        employee = new Engineer(name, id, email, github);
-
-        console.log(employee);
-      } else if (role === "Intern") {
-        employee = new Intern(name, id, email, school);
-
-        console.log(employee);
-      }
-
-      teamArray.push(employee);
-
-      if (confirmAddEmployee) {
-        return addEmployee(teamArray);
-      } else {
-        return teamArray;
+        case "Intern":
+          internPrompt().then((answers) => {
+            const { name, id, email, school } = answers;
+            const intern = new Intern(name, id, email, school);
+            team.push(intern);
+            teamPrompt();
+          });
+          break;
+        default:
+          buildTeam();
       }
     });
 };
-managerPrompt().then(emoloyeePrompt);
-//   .then((team) => {
-//     return generateHTML(team);
-//   })
-//   .then((pageHTML) => {
-//     return writeFile(pageHTML);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+
+function creatHTML(teamArr) {
+  let teamCards = "";
+
+  teamArr.forEach((teamMember) => {
+    if (teamMember.getRole() === "Manager") {
+      teamCards = teamCards + templates.generateManager(teamMember);
+    }
+    if (teamMember.getRole() === "Engineer") {
+      teamCards = teamCards + templates.generateEngineer(teamMember);
+    }
+    if (teamMember.getRole() === "Intern") {
+      teamCards = teamCards + templates.generateIntern(teamMember);
+    }
+  });
+
+  return `
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="./Assets/styles.css">
+        
+        
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="teamCards">
+                    <!--Team Cards-->
+                    ${teamCards}
+                </div>
+            </div>
+        </main>
+
+    </body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+    </html>
+  `;
+}
+
+const buildTeam = () => {
+  fs.writeFileSync(PATH, creatHTML(team), "utf-8");
+};
+
+main();
---
+++ src/templates.js
@@ -0,0 +1,65 @@
+// Manager card
+const generateManager = (manager) => {
+  const { name, id, email, officeNumber } = manager;
+
+  return `
+    <div class="col-4 m-4">
+        <div class="card h-100">
+            <div class="card-header p-3" id="managerHeader">
+                <h3>${name}</h3>
+                <i class="material-icons">person</i>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body m-2">
+                <p class="id">ID: ${id}</p>
+                <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
+                <p class="office">Office Number: ${officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// // Engineer card
+const generateEngineer = (engineer) => {
+  const { name, id, email, github } = engineer;
+
+  return `
+    <div class="col-4 m-4">
+        <div class="card h-100">
+            <div class="card-header p-3" id="engineerHeader">
+                <h3>${name}</h3>
+                <h4>Engineer</h4><i class="material-icons">engineering</i>
+            </div>
+            <div class="card-body m-2">
+                <p class="id">ID: ${id}</p>
+                <p class="email">Email: <a href="mailto:${email}">${email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${github}">${github}</a></p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// Intern card
+const generateIntern = (intern) => {
+  const { name, id, email, school } = intern;
+
+  return `
+    <div class="col-4 m-4">
+        <div class="card h-100">
+            <div class="card-header p-3" id="internHeader">
+                <h3>${name}</h3>
+                <h4>Intern</h4><i class="material-icons">school</i>
+            </div>
+            <div class="card-body m-2">
+                <p class="id">ID: ${id}</p>
+                <p class="email">Email:<a href="mailto:${email}">${email}</a></p>
+                <p class="school">School: ${school}</p>
+            </div>
+    </div>
+</div>
+    `;
+};
+
+module.exports = { generateManager, generateEngineer, generateIntern };
